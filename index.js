53aed8cb931425177610b0b107ca4070ef9e75a7
codewithowais <codewithowais@gmail.com>
2024-01-21 14:05:22 +0500

    add static route

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ const DbURL =
 
 const port = 3000;
 
-mongoose
-  .connect(DbURL)
-  .then(() => console.log("Database connected..."))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(DbURL)
+//   .then(() => console.log("Database connected..."))
+//   .catch((err) => console.log(err));
 
 app.get("/", async (req, res) => {
+  try {
+    res.send("Hello world");
+  } catch (error) {
+    console.log("users"), res.send("Something went wrong..");
+  }
+});
+
+app.get("/users", async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
@@ -23,7 +31,7 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/", async (req, res) => {
+app.post("/users", async (req, res) => {
   try {
     const userr = await User.create(req.body);
     res.json(userr);
@@ -32,7 +40,7 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.put("/:id", async (req, res) => {
+app.put("/users/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const user = await User.findByIdAndUpdate(id, req.body);
@@ -43,7 +51,7 @@ app.put("/:id", async (req, res) => {
   }
 });
 
-app.delete("/:id", async (req, res) => {
+app.delete("/users/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const user = await User.findByIdAndDelete(id);
